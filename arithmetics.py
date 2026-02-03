de4edab299840965098b88ac303724c539f7c377
Asabeneh <asabeneh@gmail.com>
2026-02-03 20:39:23 +0200

    arithmetics

---
+++ arithmetics.py
@@ -0,0 +1,18 @@
+# Here is the arithmetics
+'''
++: additon
+-: subtraction
+*: multiplication
+/: division
+%: modulus
+**: exponentiation
+//: floar division
+'''
+print('The sum is', 1 + 2)
+print('The difference is', 2 - 1)
+print('The product is', 3 * 2)
+print('The division', 3 / 2)
+print('The power is ', 2 * 2 * 2)
+print(2 ** 3)
+print('The remainder is', 5 % 2)
+print('The qotient', 7 // 4)
