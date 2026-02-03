5277581af54807fe0072bb6cef42df242f1b770f
Asabeneh <asabeneh@gmail.com>
2026-02-03 20:33:51 +0200

    python comment

---
+++ comments.py
@@ -0,0 +1,23 @@
+# this is a single line Python comment
+# Comment is useful to make the code readable and maintainable
+
+
+# the print function takes one or several inputs and it diplays it
+
+print('Hello world')
+print('I','love','people',2026)
+print('Comment is very important to make our code readable')
+
+'''
+Multiline comments which allow 
+us to comment 
+on multiple lines
+
+'''
+
+"""
+Multiline comments which allow 
+us to comment 
+on multiple lines
+
+"""
